81dc9d4431428025e414ade3449b9da75a14e02a
James Ross <git@james-ross.co.uk>
2018-08-05 17:11:53 +0100

    refactor: Tidy up using statements

--- Models/Overpass/Element.cs
+++ Models/Overpass/Element.cs
@@ -1,24 +1,9 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats.Png;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
-using SixLabors.ImageSharp.Processing.Drawing;
-using SixLabors.ImageSharp.Processing.Drawing.Pens;
-using SixLabors.Primitives;
 
 namespace osm_road_overlay.Models.Overpass
 {
-    public struct Element {
+    public struct Element
+    {
         public string type;
         public long id;
         public double lat;
--- Models/Overpass/Response.cs
+++ Models/Overpass/Response.cs
@@ -1,24 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats.Png;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
-using SixLabors.ImageSharp.Processing.Drawing;
-using SixLabors.ImageSharp.Processing.Drawing.Pens;
-using SixLabors.Primitives;
-
 namespace osm_road_overlay.Models.Overpass
 {
-    public struct Response {
+    public struct Response
+    {
         public Element[] elements;
     }
 }
