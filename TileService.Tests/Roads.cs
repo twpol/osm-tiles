a76b0d86372684ba2d617a9e134532cae28f8cd6
James Ross <git@james-ross.co.uk>
2018-08-19 17:54:48 +0100

    test: Add wiki tests for cycle lanes

--- TileService.Tests/Roads.cs
+++ TileService.Tests/Roads.cs
@@ -126,5 +126,83 @@ namespace TileService.Tests
                 GetStraightRoadText("highway=road", "oneway=yes", "lanes=4")
             );
         }
+
+        [Fact]
+        public void WikiBicycleExampleL1a()
+        {
+            const string road = "Road(Edge 0.0m|Cycle 1.0m|Car 3.0m|Car 3.0m|Cycle 1.0m|Edge 0.0m, Center=4.0m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "cycleway=lane"));
+            Assert.Equal(road, GetStraightRoadText("highway=road", "cycleway:left=lane", "cycleway:right=lane"));
+            Assert.Equal(road, GetStraightRoadText("highway=road", "cycleway:both=lane"));
+        }
+
+        [Fact]
+        public void WikiBicycleExampleL1b()
+        {
+            const string road = "Road(Edge 0.0m|Car 3.0m|Car 3.0m|Cycle 1.0m|Cycle 1.0m|Edge 0.0m, Center=3.0m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "cycleway:right=lane", "cycleway:right:oneway=no"));
+            // Can't be distinguished from L1a: Assert.Equal(road, GetStraightRoadText("highway=road", "cycleway=lane"));
+        }
+
+        [Fact]
+        public void WikiBicycleExampleL2()
+        {
+            const string road = "Road(Edge 0.0m|Car 3.0m|Car 3.0m|Cycle 1.0m|Edge 0.0m, Center=3.0m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "cycleway:right=lane"));
+        }
+
+        [Fact]
+        public void WikiBicycleExampleM1()
+        {
+            const string road = "Road(Edge 0.0m|Cycle 1.0m|Car 3.0m|Cycle 1.0m|Edge 0.0m, Center=2.5m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway=lane", "oneway:bicycle=no"));
+            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway:left=opposite_lane", "cycleway:right=lane"));
+        }
+
+        [Fact]
+        public void WikiBicycleExampleM2a()
+        {
+            const string road = "Road(Edge 0.0m|Car 3.0m|Cycle 1.0m|Edge 0.0m, Center=1.5m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway:right=lane"));
+            // Can't be distinguished from M2b: Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway=lane"));
+        }
+
+        [Fact]
+        public void WikiBicycleExampleM2b()
+        {
+            const string road = "Road(Edge 0.0m|Cycle 1.0m|Car 3.0m|Edge 0.0m, Center=2.5m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway:left=lane"));
+            // Can't be distinguished from M2a: Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway=lane"));
+        }
+
+        [Fact]
+        public void WikiBicycleExampleM2c()
+        {
+            const string road = "Road(Edge 0.0m|Car 3.0m|Cycle 1.0m|Car 3.0m|Edge 0.0m, Center=3.5m)";
+            // Can't be distinguished from L1a: Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "lanes=2", "cycleway=lane"));
+        }
+
+        [Fact]
+        public void WikiBicycleExampleM2d()
+        {
+            const string road = "Road(Edge 0.0m|Cycle 1.0m|Cycle 1.0m|Car 3.0m|Edge 0.0m, Center=3.5m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "oneway:bicycle=no", "cycleway:left=lane", "cycleway:left:oneway=no"));
+        }
+
+        [Fact]
+        public void WikiBicycleExampleM3a()
+        {
+            const string road = "Road(Edge 0.0m|Cycle 1.0m|Car 3.0m|Edge 0.0m, Center=2.5m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "oneway:bicycle=no", "cycleway:left=opposite_lane"));
+            // Can't be distinguished from M3b: Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "oneway:bicycle=no", "cycleway=opposite_lane"));
+        }
+
+        [Fact]
+        public void WikiBicycleExampleM3b()
+        {
+            const string road = "Road(Edge 0.0m|Car 3.0m|Cycle 1.0m|Edge 0.0m, Center=1.5m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "oneway:bicycle=no", "cycleway:right=opposite_lane"));
+            // Can't be distinguished from M3a: Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "oneway:bicycle=no", "cycleway=opposite_lane"));
+        }
     }
 }
