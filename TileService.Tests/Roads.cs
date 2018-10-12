2e2b475e03ad2ca07f01c1daabb48ade7094e594
James Ross <git@james-ross.co.uk>
2018-10-12 22:19:51 +0100

    test: Properly flip wiki examples for driving on left

--- TileService.Tests/Roads.cs
+++ TileService.Tests/Roads.cs
@@ -127,8 +127,8 @@ namespace TileService.Tests
             );
         }
 
-        // The following tests are based on the examples found at:
-        // https://wiki.openstreetmap.org/wiki/Bicycle
+        // The following tests are based on the examples found at: https://wiki.openstreetmap.org/wiki/Bicycle
+        // In all these tests, left/right have been flipped as wiki is using driving-on-right and we're driving-on-left.
 
         [Fact]
         public void WikiBicycleExampleL1a()
@@ -142,40 +142,39 @@ namespace TileService.Tests
         [Fact]
         public void WikiBicycleExampleL1b()
         {
-            const string road = "Road(Edge|Car ↑ 3.0m|Car ↓ 3.0m|Cycle ↑ 1.0m|Cycle ↓ 1.0m|Edge, Center=3.0m)";
-            Assert.Equal(road, GetStraightRoadText("highway=road", "cycleway:right=lane", "cycleway:right:oneway=no"));
+            const string road = "Road(Edge|Cycle ↑ 1.0m|Cycle ↓ 1.0m|Car ↑ 3.0m|Car ↓ 3.0m|Edge, Center=5.0m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "cycleway:left=lane", "cycleway:left:oneway=no"));
             // Can't be distinguished from L1a: Assert.Equal(road, GetStraightRoadText("highway=road", "cycleway=lane"));
         }
 
         [Fact]
         public void WikiBicycleExampleL2()
         {
-            const string road = "Road(Edge|Car ↑ 3.0m|Car ↓ 3.0m|Cycle ↓ 1.0m|Edge, Center=3.0m)";
-            Assert.Equal(road, GetStraightRoadText("highway=road", "cycleway:right=lane"));
+            const string road = "Road(Edge|Cycle ↑ 1.0m|Car ↑ 3.0m|Car ↓ 3.0m|Edge, Center=4.0m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "cycleway:left=lane"));
         }
 
         [Fact]
         public void WikiBicycleExampleM1()
         {
             const string road = "Road(Edge|Cycle ↑ 1.0m|Car ↑ 3.0m|Cycle ↓ 1.0m|Edge, Center=2.5m)";
-            // TODO: oneway:bicycle=no not understood: Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway=lane", "oneway:bicycle=no"));
-            // Note: Flipped left/right here because we're driving on the left, wiki example is driving on the right:
+            // TODO: oneway:bicycle=no not supported: Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway=lane", "oneway:bicycle=no"));
             Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway:left=lane", "cycleway:right=opposite_lane"));
         }
 
         [Fact]
         public void WikiBicycleExampleM2a()
         {
-            const string road = "Road(Edge|Car ↑ 3.0m|Cycle ↑ 1.0m|Edge, Center=1.5m)";
-            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway:right=lane"));
+            const string road = "Road(Edge|Cycle ↑ 1.0m|Car ↑ 3.0m|Edge, Center=2.5m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway:left=lane"));
             // Can't be distinguished from M2b: Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway=lane"));
         }
 
         [Fact]
         public void WikiBicycleExampleM2b()
         {
-            const string road = "Road(Edge|Cycle ↑ 1.0m|Car ↑ 3.0m|Edge, Center=2.5m)";
-            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway:left=lane"));
+            const string road = "Road(Edge|Car ↑ 3.0m|Cycle ↑ 1.0m|Edge, Center=1.5m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway:right=lane"));
             // Can't be distinguished from M2a: Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "cycleway=lane"));
         }
 
@@ -183,110 +182,124 @@ namespace TileService.Tests
         public void WikiBicycleExampleM2c()
         {
             const string road = "Road(Edge|Car ↑ 3.0m|Cycle ↑ 1.0m|Car ↑ 3.0m|Edge, Center=3.5m)";
-            // Can't be distinguished from L1a: Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "lanes=2", "cycleway=lane"));
+            // Ambigious with a cycleway on the left/right side of the road: Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "lanes=2", "cycleway=lane"));
         }
 
         [Fact]
         public void WikiBicycleExampleM2d()
         {
-            const string road = "Road(Edge|Cycle ↑ 1.0m|Cycle ↓ 1.0m|Car ↑ 3.0m|Edge, Center=3.5m)";
-            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "oneway:bicycle=no", "cycleway:left=lane", "cycleway:left:oneway=no"));
+            const string road = "Road(Edge|Car ↑ 3.0m|Cycle ↑ 1.0m|Cycle ↓ 1.0m|Edge, Center=1.5m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "oneway:bicycle=no", "cycleway:right=lane", "cycleway:right:oneway=no"));
         }
 
         [Fact]
         public void WikiBicycleExampleM3a()
         {
-            const string road = "Road(Edge|Cycle ↓ 1.0m|Car ↑ 3.0m|Edge, Center=2.5m)";
-            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "oneway:bicycle=no", "cycleway:left=opposite_lane"));
+            const string road = "Road(Edge|Car ↑ 3.0m|Cycle ↓ 1.0m|Edge, Center=1.5m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "oneway:bicycle=no", "cycleway:right=opposite_lane"));
             // Can't be distinguished from M3b: Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "oneway:bicycle=no", "cycleway=opposite_lane"));
         }
 
         [Fact]
         public void WikiBicycleExampleM3b()
         {
-            const string road = "Road(Edge|Car ↑ 3.0m|Cycle ↓ 1.0m|Edge, Center=1.5m)";
-            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "oneway:bicycle=no", "cycleway:right=opposite_lane"));
+            const string road = "Road(Edge|Cycle ↓ 1.0m|Car ↑ 3.0m|Edge, Center=2.5m)";
+            Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "oneway:bicycle=no", "cycleway:left=opposite_lane"));
             // Can't be distinguished from M3a: Assert.Equal(road, GetStraightRoadText("highway=road", "oneway=yes", "oneway:bicycle=no", "cycleway=opposite_lane"));
         }
 
-        // [Fact]
-        // public void WikiBicycleExampleT1MultiWay1()
-        // {
-        //     Assert.Equal(
-        //         "Road(Edge|Car ↑ 3.0m|Car ↓ 3.0m|Edge, Center=3.0m)",
-        //         GetStraightRoadText("highway=road", "bicycle=use_sidepath")
-        //     );
-        // }
+        // WikiBicycleExampleM4 is comprised entirely of other examples
 
-        // [Fact]
-        // public void WikiBicycleExampleT1MultiWay2()
-        // {
-        //     Assert.Equal(
-        //         "Road(Edge|Cycle ↑ 1.0m|Edge, Center=0.5m)",
-        //         GetStraightRoadText("highway=cycleway", "oneway=yes")
-        //     );
-        // }
+        // WikiBicycleExampleM5 is comprised entirely of other examples
 
-        // [Fact]
-        // public void WikiBicycleExampleT1SingleWay()
-        // {
-        //     Assert.Equal(
-        //         "Road(Edge|Cycle ↑ 1.0m|Verge 0.5m|Car ↑ 3.0m|Car ↓ 3.0m|Verge 0.5m|Cycle ↓ 1.0m|Edge, Center=4.5m)",
-        //         GetStraightRoadText("highway=road", "cycleway=track")
-        //     );
-        // }
+        [Fact]
+        public void WikiBicycleExampleT1MultiWayA()
+        {
+            Assert.Equal(
+                "Road(Edge|Car ↑ 3.0m|Car ↓ 3.0m|Edge, Center=3.0m)",
+                GetStraightRoadText("highway=road", "bicycle=use_sidepath")
+            );
+        }
 
-        // // WikiBicycleExampleT2MultiWay1 would be identical to WikiBicycleExampleT1MultiWay1
+        [Fact]
+        public void WikiBicycleExampleT1MultiWayBC()
+        {
+            // TODO: highway=cycleway not supported:
+            // Assert.Equal(
+            //     "Road(Edge|Cycle ↑ 1.0m|Edge, Center=0.5m)",
+            //     GetStraightRoadText("highway=cycleway", "oneway=yes")
+            // );
+        }
 
-        // [Fact]
-        // public void WikiBicycleExampleT2MultiWay2()
-        // {
-        //     Assert.Equal(
-        //         "Road(Edge|Cycle ↑ 1.0m|Cycle ↓ 1.0m|Edge, Center=1.0m)",
-        //         GetStraightRoadText("highway=cycleway", "oneway=no")
-        //     );
-        // }
+        [Fact]
+        public void WikiBicycleExampleT1SingleWay()
+        {
+            // TODO: cycleway=track not supported:
+            // Assert.Equal(
+            //     "Road(Edge|Cycle ↑ 1.0m|Verge 0.5m|Car ↑ 3.0m|Car ↓ 3.0m|Verge 0.5m|Cycle ↓ 1.0m|Edge, Center=4.5m)",
+            //     GetStraightRoadText("highway=road", "cycleway=track")
+            // );
+        }
 
-        // [Fact]
-        // public void WikiBicycleExampleT2SingleWay()
-        // {
-        //     Assert.Equal(
-        //         "Road(Edge|Car ↑ 3.0m|Car ↓ 3.0m|Verge 0.5m|Cycle ↑ 1.0m|Cycle ↓ 1.0m|Edge, Center=3.0m)",
-        //         GetStraightRoadText("highway=road", "cycleway:right=track", "cycleway:right:oneway=no")
-        //     );
-        // }
+        // WikiBicycleExampleT2MultiWayA is identical to WikiBicycleExampleT1MultiWayA
 
-        // [Fact]
-        // public void WikiBicycleExampleT3MultiWay1()
-        // {
-        //     Assert.Equal(
-        //         "Road(Edge|Car ↑ 3.0m|Edge, Center=1.5m)",
-        //         GetStraightRoadText("highway=road", "oneway=yes", "bicycle=use_sidepath")
-        //     );
-        // }
+        [Fact]
+        public void WikiBicycleExampleT2MultiWayB()
+        {
+            // TODO: highway=cycleway not supported:
+            // Assert.Equal(
+            //     "Road(Edge|Cycle ↑ 1.0m|Cycle ↓ 1.0m|Edge, Center=1.0m)",
+            //     GetStraightRoadText("highway=cycleway", "oneway=no")
+            // );
+        }
 
-        // // WikiBicycleExampleT3MultiWay2 would be identical to WikiBicycleExampleT2MultiWay2
+        [Fact]
+        public void WikiBicycleExampleT2SingleWay()
+        {
+            // TODO: cycleway=track not supported:
+            // Assert.Equal(
+            //     "Road(Edge|Cycle ↑ 1.0m|Cycle ↓ 1.0m|Verge 0.5m|Car ↑ 3.0m|Car ↓ 3.0m|Edge, Center=5.5m)",
+            //     GetStraightRoadText("highway=road", "cycleway:left=track", "cycleway:left:oneway=no")
+            // );
+        }
 
-        // [Fact]
-        // public void WikiBicycleExampleT3SingleWay()
-        // {
-        //     Assert.Equal(
-        //         "Road(Edge|Car ↑ 3.0m|Verge 0.5m|Cycle ↑ 1.0m|Cycle ↓ 1.0m|Edge, Center=1.5m)",
-        //         GetStraightRoadText("highway=road", "oneway=yes", "cycleway:right=track", "oneway:bicycle=no")
-        //     );
-        // }
+        [Fact]
+        public void WikiBicycleExampleT3MultiWayA()
+        {
+            Assert.Equal(
+                "Road(Edge|Car ↑ 3.0m|Edge, Center=1.5m)",
+                GetStraightRoadText("highway=road", "oneway=yes", "bicycle=use_sidepath")
+            );
+        }
 
-        // // WikiBicycleExampleT4MultiWay1 would be identical to WikiBicycleExampleT1MultiWay1
+        // WikiBicycleExampleT3MultiWayB is identical to WikiBicycleExampleT2MultiWayB
 
-        // // WikiBicycleExampleT4MultiWay2 would be identical to WikiBicycleExampleT1MultiWay2
+        [Fact]
+        public void WikiBicycleExampleT3SingleWay()
+        {
+            // TODO: cycleway=track not supported:
+            // Assert.Equal(
+            //     "Road(Edge|Cycle ↑ 1.0m|Cycle ↓ 1.0m|Verge 0.5m|Car ↑ 3.0m|Edge, Center=4.0m)",
+            //     GetStraightRoadText("highway=road", "oneway=yes", "cycleway:left=track", "oneway:bicycle=no")
+            // );
+        }
 
-        // [Fact]
-        // public void WikiBicycleExampleT4SingleWay()
-        // {
-        //     Assert.Equal(
-        //         "Road(Edge|Car ↑ 3.0m|Car ↓ 3.0m|Verge 0.5m|Cycle ↓ 1.0m|Edge, Center=3.0m)",
-        //         GetStraightRoadText("highway=road", "cycleway:right=track")
-        //     );
-        // }
+        // WikiBicycleExampleT4MultiWayA is identical to WikiBicycleExampleT1MultiWayA
+
+        // WikiBicycleExampleT4MultiWayB is identical to WikiBicycleExampleT1MultiWayB
+
+        [Fact]
+        public void WikiBicycleExampleT4SingleWay()
+        {
+            // TODO: cycleway=track not supported:
+            // Assert.Equal(
+            //     "Road(Edge|Cycle ↓ 1.0m|Verge 0.5m|Car ↑ 3.0m|Car ↓ 3.0m|Edge, Center=4.5m)",
+            //     GetStraightRoadText("highway=road", "cycleway:left=track")
+            // );
+        }
+
+        // TODO: Examples S1 through S5
+
+        // TODO: Examples B1 through B6
     }
 }
