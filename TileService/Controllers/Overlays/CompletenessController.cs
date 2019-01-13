8f02f07cf03b393f69c90ef92530b5d357fd5adc
James Ross <git@james-ross.co.uk>
2019-01-13 22:45:25 +0000

    feat: Add completeness tiles (somewhat dodgy still)

---
+++ TileService/Controllers/Overlays/CompletenessController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using TileService.Models.Geometry;
+using PointF = SixLabors.Primitives.PointF;
+
+namespace TileService.Controllers.Overlays
+{
+    [Route("overlays/completeness")]
+    public class CompletenessController : Controller
+    {
+        const int ZoomMinimum = 10;
+        const int ZoomMaximum = 10;
+
+        // GET overlays/completeness/:zoom/:x/:y.png
+        [HttpGet("{zoom}/{x}/{y}.png")]
+        public async Task<ActionResult> Get(int zoom, int x, int y)
+        {
+            if (zoom < ZoomMinimum || zoom > ZoomMaximum) {
+                return BadRequest();
+            }
+
+            var start = DateTimeOffset.UtcNow;
+
+            var tile = await CompletenessTile.Cache.Get(zoom, x, y);
+
+            var image = new Image<Rgba32>(256, 256);
+            image.Mutate(context =>
+            {
+                var color = new Rgba32(1 - tile.Completeness, tile.Completeness, 0, 0.5f);
+                context.FillPolygon(
+                    color,
+                    new PointF(0, 0),
+                    new PointF(256, 0),
+                    new PointF(256, 256),
+                    new PointF(0, 256)
+                );
+            });
+
+            var end = DateTimeOffset.UtcNow;
+            Console.WriteLine($"Rendered {tile} in {(end - start).TotalMilliseconds:F0} ms");
+
+            var stream = new MemoryStream();
+            image.SaveAsPng(stream);
+            stream.Position = 0;
+
+            HttpContext.Response.Headers.Add("Cache-Control", new[] { "public", "max-age=43200" });
+
+            return File(stream, "image/png");
+        }
+    }
+}
--- TileService/Models/Overpass/Query.cs
+++ TileService/Models/Overpass/Query.cs
@@ -13,12 +13,21 @@ namespace TileService.Models.Overpass
 
         static readonly HttpClient Client = new HttpClient();
 
+        public static async Task<Response> GetCompleteness(Tile tile)
+        {
+            var bbox = GetBoundingBoxFromTile(tile, 20);
+            return await RunQuery($"[out:json][timeout:60];nwr[\"natural\"]({bbox});out count;nwr[\"landuse\"]({bbox});out count;nwr[\"barrier\"]({bbox});out count;nwr[\"waterway\"]({bbox});out count;nwr[\"highway\"=\"road\"]({bbox});out count;nwr[\"highway\"!=\"road\"]({bbox});out count;nwr[\"cycleway\"]({bbox});out count;nwr[\"power\"]({bbox});out count;nwr[\"building\"]({bbox});out count;nwr[\"building\"][\"addr:street\"]({bbox});out count;nwr[\"building\"][\"addr:postcode\"]({bbox});out count;nwr[\"amenity\"]({bbox});out count;");
+        }
+
         public static async Task<Response> GetHighways(Tile tile)
         {
             // Gather the bounding box with 20m extra around it for capturing edges.
-            var bbox = GetBoundingBoxFromTile(tile);
-            var overpassQuery = $"[out:json][timeout:60];(way[\"highway\"]({bbox}););out body;>;out skel qt;";
+            var bbox = GetBoundingBoxFromTile(tile, 20);
+            return await RunQuery($"[out:json][timeout:60];(way[\"highway\"]({bbox}););out body;>;out skel qt;");
+        }
 
+        static async Task<Response> RunQuery(string overpassQuery)
+        {
             using (var overpassResponse = await Client.PostAsync(
                 OverpassAPIEndpoint,
                 new FormUrlEncodedContent(new Dictionary<string, string>() {
@@ -33,9 +42,9 @@ namespace TileService.Models.Overpass
             }
         }
 
-        static string GetBoundingBoxFromTile(Tile tile)
+        static string GetBoundingBoxFromTile(Tile tile, int oversizeM = 0)
         {
-            var oversizeScale = 20 * tile.ImageScale / 256;
+            var oversizeScale = oversizeM * tile.ImageScale / 256;
             var latExtra = oversizeScale * (tile.NW.Lat - tile.SE.Lat);
             var lonExtra = oversizeScale * (tile.SE.Lon - tile.NW.Lon);
             return $"{tile.SE.Lat - latExtra},{tile.NW.Lon - lonExtra},{tile.NW.Lat + latExtra},{tile.SE.Lon + lonExtra}";
