d2a89192ec21c86a729ab7f360d0a5e61d42dbfc
James Ross <git@james-ross.co.uk>
2023-07-18 00:10:40 +0100

    feat: Visualize oneway arrows
    Closes #23

--- TileService/Models/Geometry/Road.cs
+++ TileService/Models/Geometry/Road.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 
 namespace TileService.Models.Geometry
 {
@@ -7,6 +8,12 @@ namespace TileService.Models.Geometry
     {
         public ImmutableList<Lane> Lanes { get; }
         public float Center { get; }
+
+        public bool IsOneWay(LaneType type)
+        {
+            var directions = Lanes.Where(lane => lane.Type == type).Select(lane => lane.Direction).Distinct().ToList();
+            return directions.Count == 1 && directions[0] != LaneDirection.Both;
+        }
 
         public Road(IEnumerable<Lane> lanes, float center)
         {
